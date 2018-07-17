15ba791e83003a94ad6e3b3afeed7a2565b73ad0
Alexsandro Vítor <avsc@cin.ufpe.br>
2018-07-17 20:55:35 -0300

    Geracao de raios implementada

--- Main.py
+++ Main.py
@@ -2,20 +2,9 @@
 import numpy as np
 import cv2
 from tkinter.filedialog import askopenfilename
+
 import Scene
 import Object
-
-sceneFile = askopenfilename(filetypes=[("Scene Files","*.sdl")])
-scene = Scene.Scene(sceneFile)
-
-# objFile = askopenfilename(filetypes=[("Object Files","*.obj")])
-# object = Object.Object(objFile)
-print(scene)
-
-img = np.zeros([400, 200, 3])
-img[100, 20:100, 0] = 255
-img[200, 20:100, 1] = 255
-img[300, 20:100, 2] = 255
 
 def showimg(title, img):
 	'''Como o opencv usa (y, x) como ordem das coordenadas e [blue, green, red] como ordem das cores, essa função é usada para visualizar uma matriz mais "convencional" como uma imagem do opencv.'''
@@ -24,6 +13,18 @@ def showimg(title, img):
 	g = np.transpose(g)
 	b = np.transpose(b)
 	cv2.imshow(title, cv2.merge((b,g,r)))
+
+sceneFile = askopenfilename(filetypes=[("Scene Files","*.sdl")])
+scene = Scene.Scene(sceneFile)
+
+# objFile = askopenfilename(filetypes=[("Object Files","*.obj")])
+# object = Object.Object(objFile)
+print(scene)
+
+img = np.zeros([scene.size[0], scene.size[1], 3])
+img[50, 20:100, 0] = 255
+img[100, 20:100, 1] = 255
+img[120, 20:100, 2] = 255
 
 showimg("Teste", img)
 cv2.waitKey(0)
--- Scene.py
+++ Scene.py
@@ -1,4 +1,7 @@
 # -*- encoding: utf-8 -*-
+import numpy as np
+import Object
+
 class Scene:
 	def __init__(self, filename):
 		self.light = []
@@ -19,7 +22,7 @@ class Scene:
 				elif line[0] == "ambient":
 					self.ambient = line[1]
 				elif line[0] == "light":
-					newLight = [line[1]]
+					newLight = [Object.Object("Objects/" + line[1])]
 					newLight.extend(list(map(float, line[2:])))
 					self.light.append(newLight)
 				elif line[0] == "npaths":
@@ -29,9 +32,29 @@ class Scene:
 				elif line[0] == "seed":
 					self.seed = line[1]
 				elif line[0] == "object":
-					newObject = [line[1]]
+					newObject = [Object.Object("Objects/" + line[1])]
 					newObject.extend(list(map(float, line[2:])))
 					self.object.append(newObject)
+	
 	def __str__(self):
 		'''Esse metodo é chamado em print(scene)'''
 		return "Scene(\n\toutput = " + self.output + "\n\teye = " + str(self.eye) + "\n\tortho = " + str(self.ortho) + "\n\tsize = " + str(self.size) + "\n\tbackground = " + str(self.background) + "\n\tambient = " + str(self.ambient) + "\n\tlight = " + str(self.light) + "\n\tnpaths = " + str(self.npaths) + "\n\ttonemapping = " + str(self.tonemapping) + "\n\tseed = " + str(self.seed) + "\n\tobject = " + str(self.object) + "\n)"
+	
+	def scale_screen_camera(self, x, y):
+		'''Converte pontos na tela em pontos na cena'''
+		tX = x / (self.size[0] - 1)
+		tY = y / (self.size[1] - 1)
+		return [(1 - tX) * self.ortho[0] + tX * self.ortho[2], (1 - tY) * self.ortho[3] + tY * self.ortho[1], 0]
+	
+	def gen_ray_vectors(self):
+		'''Gera as direções dos raios de cada pixel da tela'''
+		output = np.zeros([self.size[0], self.size[1], 3])
+		for x in range(output.shape[0]):
+			for y in range(output.shape[1]):
+				output[x, y, :] = np.subtract(self.scale_screen_camera(x, y), self.eye)
+		return output
+	
+	def path_tracing(self):
+		'''O código do path tracing vai aqui'''
+		img = np.zeros([self.size[0], self.size[1], 3])
+		return img
