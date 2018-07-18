1d945e7965a934dc4f6dd7b09d17868100b4f93f
Alexsandro Vítor <avsc@cin.ufpe.br>
2018-07-17 22:05:00 -0300

    Implementado produto cruzado 4D

--- Main.py
+++ Main.py
@@ -5,14 +5,7 @@ from tkinter.filedialog import askopenfilename
 
 import Scene
 import Object
-
-def showimg(title, img):
-	'''Como o opencv usa (y, x) como ordem das coordenadas e [blue, green, red] como ordem das cores, essa função é usada para visualizar uma matriz mais "convencional" como uma imagem do opencv.'''
-	r,g,b = cv2.split(img)
-	r = np.transpose(r)
-	g = np.transpose(g)
-	b = np.transpose(b)
-	cv2.imshow(title, cv2.merge((b,g,r)))
+import Util
 
 sceneFile = askopenfilename(filetypes=[("Scene Files","*.sdl")])
 scene = Scene.Scene(sceneFile)
@@ -21,11 +14,14 @@ scene = Scene.Scene(sceneFile)
 # object = Object.Object(objFile)
 print(scene)
 
+print(Util.cross_3d([2, 4, 5, 1], [1, 4, 2, 1], [1, 7, 4, 1]))
+print(np.cross(np.subtract([2, 4, 5], [1, 4, 2]), np.subtract([2, 4, 5], [1, 7, 4])))
+
 img = np.zeros([scene.size[0], scene.size[1], 3])
 img[50, 20:100, 0] = 255
 img[100, 20:100, 1] = 255
 img[120, 20:100, 2] = 255
 
-showimg("Teste", img)
+Util.showimg("Teste", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ Util.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+import numpy as np
+import numpy.linalg as alg
+import cv2
+
+def cross_3d(a, b, c):
+	'''Produto cruzado para 3 pontos 4D (o cross do numpy só vai até 3D, então tive que fazer)'''
+	i = np.array([a[1:], b[1:], c[1:]])
+	j = np.array([[a[2], a[3], a[0]], [b[2], b[3], b[0]], [c[2], c[3], c[0]]])
+	k = np.array([[a[3], a[0], a[1]], [b[3], b[0], b[1]], [c[3], c[0], c[1]]])
+	l = np.array([a[:-1], b[:-1], c[:-1]])
+	return [alg.det(i), -alg.det(j), alg.det(k), -alg.det(l)]
+
+def showimg(title, img):
+	'''Como o opencv usa (y, x) como ordem das coordenadas e [blue, green, red] como ordem das cores, essa função é usada para visualizar uma matriz mais "convencional" como uma imagem do opencv.'''
+	r,g,b = cv2.split(img)
+	r = np.transpose(r)
+	g = np.transpose(g)
+	b = np.transpose(b)
+	cv2.imshow(title, cv2.merge((b,g,r)))
